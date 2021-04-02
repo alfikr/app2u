bf4fbba11fb92c971adf6860bd80d6ebc2c8f048
alfikr <daffodil1411@gmail.com>
2021-04-02 18:53:14 +0700

    fixing file player

---
+++ lib/component/youtube_player/YoutubePlayer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class YoutubePlayer extends StatefulWidget {
+  YoutubePlayer({Key key}) : super(key: key);
+
+  @override
+  _YoutubePlayerState createState() => _YoutubePlayerState();
+}
+
+class _YoutubePlayerState extends State<YoutubePlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
